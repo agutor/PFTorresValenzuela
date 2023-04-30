8b8223366dc659f8aad8595bbd927d42eb10ce26
Agustin Torres Valenzuela <1997.agustorres@gmail.com>
2023-04-30 19:42:18 -0300

    storage

--- js/main copy.js
+++ js/main copy.js
@@ -1,109 +1,212 @@
-let numLista = 1
+// let numLista = 1
 
-//array de tareas del usuario
-const tareas = [ ]
+// //array de tareas del usuario
+// const tareas = [ ]
 
-//objetos que van dentro del array
-class toDoTarea {
-    constructor(numLista, tarea, dificultad) {
-        this.numLista = numLista
-        this.tarea = tarea;
-        this.dificultad = dificultad;
-    }
-}
+// //objetos que van dentro del array
+// class toDoTarea {
+//     constructor(numLista, tarea, dificultad) {
+//         this.numLista = numLista
+//         this.tarea = tarea;
+//         this.dificultad = dificultad;
+//     }
+// }
 
-//ordena las tareas del usuario en una string para mostrarselas en el ultimo alert
-function listadorDeTareas(){
-    let listadoDeTareas = "";
-    for(let i = 0; i < tareas.length; i++){
-    let listado = tareas[i];
-    let tareasListadas = "\n" + listado.numLista + ".º tarea: " + listado.tarea + "\nNivel de Dificultad: " + listado.dificultad + "\n";
-    listadoDeTareas += tareasListadas ;
-    }
-    return listadoDeTareas
-}
+// //ordena las tareas del usuario en una string para mostrarselas en el ultimo alert
+// function listadorDeTareas(){
+//     let listadoDeTareas = "";
+//     for(let i = 0; i < tareas.length; i++){
+//     let listado = tareas[i];
+//     let tareasListadas = "\n" + listado.numLista + ".º tarea: " + listado.tarea + "\nNivel de Dificultad: " + listado.dificultad + "\n";
+//     listadoDeTareas += tareasListadas ;
+//     }
+//     return listadoDeTareas
+// }
 
-//valida que los caracteres son ingrasados son letras
-function validarLetras(letra) {
-    let letraValida = /^[a-zA-Z]+$/;
-    while (!letraValida.test(letra)) {
-      letra = prompt("El dato ingresado no es válido, solo se permiten letras");
-    }
-    return letra;
-  }
+// //valida que los caracteres son ingrasados son letras
+// function validarLetras(letra) {
+//     let letraValida = /^[a-zA-Z]+$/;
+//     while (!letraValida.test(letra)) {
+//       letra = prompt("El dato ingresado no es válido, solo se permiten letras");
+//     }
+//     return letra;
+//   }
   
 
-//valida que los caracteres ingresados son numeros y que esten en el rango solicitado
-function validarNumeros(num, a, b){
-    while (isNaN(num) || num < a || num > b ) {
-        num = parseInt(prompt("El número ingresado no es válido. Ingresa un número del " + a + " al " + b))
+// //valida que los caracteres ingresados son numeros y que esten en el rango solicitado
+// function validarNumeros(num, a, b){
+//     while (isNaN(num) || num < a || num > b ) {
+//         num = parseInt(prompt("El número ingresado no es válido. Ingresa un número del " + a + " al " + b))
+//     }
+//     return num;
+// }
+
+// // comienzo de la app
+// let nombre = prompt("¡Hola! ¿Cuál es tu nombre?");
+// nombre = validarLetras(nombre);
+// let numTareas = parseInt(prompt("¡Hola " + nombre + "! ¿Cuántas tareas tienes que realizar el día de hoy? Limite de tareas: 15"))
+// numTareas = validarNumeros(numTareas, 1, 15)
+
+// //si el numero de tareas es menor o igual que tres indica que es un dia ligero o de lo contrario un dia atareado
+// //el bucle while esta para que el primer prompt contenga el mensaje indicando si son muchas tareas o no 
+// if (numTareas <= 3) {
+//     let tarea = prompt("¡Entonces hoy tenemos un día ligero! ¿Cuál es la tarea n.º" + numLista + "?");
+//     let dificultad = parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
+//     dificultad = validarNumeros(dificultad, 1, 5)
+//     const toDo = new toDoTarea(numLista, tarea, dificultad)
+//     tareas.push(toDo);
+//     while (numLista < numTareas) {
+//         numLista++;
+//         tarea = prompt("¿La tarea n.º" + numLista + "?");
+//         let dificultad = parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
+//         dificultad = validarNumeros(dificultad, 1, 5)
+//         const toDo = new toDoTarea(numLista, tarea, dificultad)
+//         tareas.push(toDo);
+//     }
+//     listadoDeTareas = listadorDeTareas();
+//     alert("¡Acá están listadas todas las tareas! \n" + listadoDeTareas + "\n¡Que tengas un día muy productivo, " + nombre + "!" + "\n \n\n PD: Mas detalles en la consola.");
+// } else {
+//     let tarea = prompt("¿Un día movido? ¡Entonces hay que empezar con energías! ¿Cuál es la tarea n.º" + numLista + "?");
+//     let dificultad =  parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
+//     dificultad = validarNumeros(dificultad, 1, 5)
+//     const toDo = new toDoTarea(numLista, tarea, dificultad)
+//     tareas.push(toDo);
+//     while (numLista < numTareas) {
+//         numLista++;
+//         tarea = prompt("¿La tarea n.º" + numLista + "?");
+//         let dificultad =  parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
+//         dificultad = validarNumeros(dificultad, 1, 5)
+//         const toDo = new toDoTarea(numLista, tarea, dificultad)
+//         tareas.push(toDo);
+//     }
+//     listadoDeTareas = listadorDeTareas()
+//     alert("¡Acá están listadas todas las tareas! \n" + listadoDeTareas + "\n¡Que tengas un día muy productivo, " + nombre + "!" + "\n \n\n PD: Mas detalles en la consola.");
+// }
+
+// const usuario = {
+//     nombreUsuario : nombre,
+//     cantidadTareas : numTareas,
+//     listado : tareas,
+// }
+
+// // aca se enlistan algunos datos porporcionados por el usuario y almacenados en el objeto "usuario" + la variable listadoDeTareas 
+// // que contiene la string generada de la funcion listadorDeTareas
+// console.log("Usuario: " + usuario.nombreUsuario)
+// console.log("Cantidad de Tareas: " + usuario.cantidadTareas)
+// console.log("Lista de tareas:\n" + listadoDeTareas)
+// console.log(tareas)
+
+// //este es un enlistado de las tareas utilizando .forEach()
+// tareas.forEach((listar) => {
+//     console.log(listar.numLista + ".º tarea: " + listar.tarea)
+// });
+
+// // y este es un enlistado utilizando .map()
+// const listador = tareas.map ((toDoList) =>{
+//     return toDoList.numLista + ".º tarea: " + toDoList.tarea;
+// })
+
+// console.log (listador)
+
+function cargarListaDeTareasDesdeLS() {
+    if (getListaDeTareas) {
+        getListaDeTareas.forEach((tarea) => {
+            const item = document.createElement("li");
+            item.innerHTML =
+                tarea.actividad +
+                " (Dificultad : " +
+                tarea.dificultad +
+                ") " +
+                "<input class='form-check-input' type='checkbox' name='asunto'></input>";
+            let checkbox = item.querySelector("input[type='checkbox']");
+            checkbox.addEventListener("click", function () {
+                if (checkbox.checked) {
+                    item.classList.add("completado");
+                    deleteBtn.style.display = "none"
+                    tareasCompletas.append(item)
+        
+                    // Mover la tarea de listaDeTareas a listaDeTareasCompletas
+                    const tareaCompletada = getListaDeTareas.splice(getListaDeTareas.indexOf(tarea), 1)[0];
+                    getListaDeTareasCompletas.push(tareaCompletada);
+                    guardarGetEnLS()
+                } else {
+                    item.classList.remove("completado");
+                    deleteBtn.style.display = "inline"
+                    listaTareas.append(item)
+                    // Mover la tarea de listaDeTareasCompletas a listaDeTareas
+                    const tareaIncompleta = getListaDeTareasCompletas.splice(getListaDeTareasCompletas.indexOf(tarea), 1)[0];
+                    getListaDeTareas.push(tareaIncompleta);
+                    guardarGetEnLS()
+                }
+                onOffDeleteBtn();
+            });
+            const deleteBtn = document.createElement("i");
+            deleteBtn.className = "fa-solid fa-delete-left fa-2xl";
+            deleteBtn.addEventListener("click", (e) => {
+                const item = e.target.parentElement;
+                item.remove(item);
+                listaDeTareas.splice(
+                    listaDeTareas.findIndex((t) => t.actividad === tarea.actividad),
+                    1
+                )[0];
+                guardarGetEnLS();
+                onOffDeleteBtn();
+            });
+            item.append(deleteBtn);
+            listaTareas.append(item);
+            onOffDeleteBtn();
+        });
     }
-    return num;
 }
-
-// comienzo de la app
-let nombre = prompt("¡Hola! ¿Cuál es tu nombre?");
-nombre = validarLetras(nombre);
-let numTareas = parseInt(prompt("¡Hola " + nombre + "! ¿Cuántas tareas tienes que realizar el día de hoy? Limite de tareas: 15"))
-numTareas = validarNumeros(numTareas, 1, 15)
-
-//si el numero de tareas es menor o igual que tres indica que es un dia ligero o de lo contrario un dia atareado
-//el bucle while esta para que el primer prompt contenga el mensaje indicando si son muchas tareas o no 
-if (numTareas <= 3) {
-    let tarea = prompt("¡Entonces hoy tenemos un día ligero! ¿Cuál es la tarea n.º" + numLista + "?");
-    let dificultad = parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
-    dificultad = validarNumeros(dificultad, 1, 5)
-    const toDo = new toDoTarea(numLista, tarea, dificultad)
-    tareas.push(toDo);
-    while (numLista < numTareas) {
-        numLista++;
-        tarea = prompt("¿La tarea n.º" + numLista + "?");
-        let dificultad = parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
-        dificultad = validarNumeros(dificultad, 1, 5)
-        const toDo = new toDoTarea(numLista, tarea, dificultad)
-        tareas.push(toDo);
+function cargarListaDeTareasCompletasDesdeLS() {
+    if (getListaDeTareasCompletas) {
+        getListaDeTareasCompletas.forEach((tarea) => {
+            const item = document.createElement("li");
+            item.innerHTML =
+                tarea.actividad +
+                " (Dificultad : " +
+                tarea.dificultad +
+                ") " +
+                "<input class='form-check-input' type='checkbox' name='asunto'></input>";
+            let checkbox = item.querySelector("input[type='checkbox']");
+            checkbox.addEventListener("click", function () {
+                if (checkbox.checked) {
+                    item.classList.add("completado");
+                    deleteBtn.style.display = "none"
+                    tareasCompletas.append(item)
+        
+                    // Mover la tarea de listaDeTareas a listaDeTareasCompletas
+                    const tareaCompletada = getListaDeTareas.splice(getListaDeTareas.indexOf(tarea), 1)[0];
+                    getListaDeTareasCompletas.push(tareaCompletada);
+                    guardarGetEnLS()
+                } else {
+                    item.classList.remove("completado");
+                    deleteBtn.style.display = "inline"
+                    listaTareas.append(item)
+                    // Mover la tarea de listaDeTareasCompletas a listaDeTareas
+                    const tareaIncompleta = getListaDeTareasCompletas.splice(getListaDeTareasCompletas.indexOf(tarea), 1)[0];
+                    getListaDeTareas.push(tareaIncompleta);
+                    guardarGetEnLS()
+                }
+                onOffDeleteBtn();
+            });
+            const deleteBtn = document.createElement("i");
+            deleteBtn.className = "fa-solid fa-delete-left fa-2xl";
+            deleteBtn.addEventListener("click", (e) => {
+                const item = e.target.parentElement;
+                item.remove(item);
+                listaDeTareas.splice(
+                    listaDeTareas.findIndex((t) => t.actividad === tarea.actividad),
+                    1
+                )[0];
+                guardarGetEnLS();
+                onOffDeleteBtn();
+            });
+            item.append(deleteBtn);
+            tareasCompletas.append(item);
+            onOffDeleteBtn();
+        });
     }
-    listadoDeTareas = listadorDeTareas();
-    alert("¡Acá están listadas todas las tareas! \n" + listadoDeTareas + "\n¡Que tengas un día muy productivo, " + nombre + "!" + "\n \n\n PD: Mas detalles en la consola.");
-} else {
-    let tarea = prompt("¿Un día movido? ¡Entonces hay que empezar con energías! ¿Cuál es la tarea n.º" + numLista + "?");
-    let dificultad =  parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
-    dificultad = validarNumeros(dificultad, 1, 5)
-    const toDo = new toDoTarea(numLista, tarea, dificultad)
-    tareas.push(toDo);
-    while (numLista < numTareas) {
-        numLista++;
-        tarea = prompt("¿La tarea n.º" + numLista + "?");
-        let dificultad =  parseInt(prompt("Del 1 al 5, ¿qué nivel de dificultad le das a esta tarea?"));
-        dificultad = validarNumeros(dificultad, 1, 5)
-        const toDo = new toDoTarea(numLista, tarea, dificultad)
-        tareas.push(toDo);
-    }
-    listadoDeTareas = listadorDeTareas()
-    alert("¡Acá están listadas todas las tareas! \n" + listadoDeTareas + "\n¡Que tengas un día muy productivo, " + nombre + "!" + "\n \n\n PD: Mas detalles en la consola.");
 }
-
-const usuario = {
-    nombreUsuario : nombre,
-    cantidadTareas : numTareas,
-    listado : tareas,
-}
-
-// aca se enlistan algunos datos porporcionados por el usuario y almacenados en el objeto "usuario" + la variable listadoDeTareas 
-// que contiene la string generada de la funcion listadorDeTareas
-console.log("Usuario: " + usuario.nombreUsuario)
-console.log("Cantidad de Tareas: " + usuario.cantidadTareas)
-console.log("Lista de tareas:\n" + listadoDeTareas)
-console.log(tareas)
-
-//este es un enlistado de las tareas utilizando .forEach()
-tareas.forEach((listar) => {
-    console.log(listar.numLista + ".º tarea: " + listar.tarea)
-});
-
-// y este es un enlistado utilizando .map()
-const listador = tareas.map ((toDoList) =>{
-    return toDoList.numLista + ".º tarea: " + toDoList.tarea;
-})
-
-console.log (listador)
+cargarListaDeTareasDesdeLS();
+cargarListaDeTareasCompletasDesdeLS();
--- js/main.js
+++ js/main.js
@@ -3,7 +3,134 @@ const selectorDificultad = document.querySelector("#select-dificultad")
 const btnAgregar = document.querySelector("#agregar-form")
 const listaTareas = document.querySelector("#lista-tareas")
 const tareasCompletas = document.querySelector("#tareas-completas")
-const xBtn =  document.querySelector("#deleteAll")
+const xBtn = document.querySelector("#deleteAll")
+
+
+
+
+// const getListaDeTareas = JSON.parse(localStorage.getItem("listaDeTareas"))
+// const getListaDeTareasCompletas = JSON.parse(localStorage.getItem("listaDeTareasCompletas"))
+
+function check(){
+    const getListaDeTareas = JSON.parse(localStorage.getItem("listaDeTareas"))
+    const getListaDeTareasCompletas = JSON.parse(localStorage.getItem("listaDeTareasCompletas"))    
+    if(getListaDeTareas != null || getListaDeTareasCompletas != null){
+        listaDeTareas = getListaDeTareas
+        listaDeTareasCompletas = getListaDeTareasCompletas
+        recargaDOM()
+    }
+    else{
+        listaDeTareas = [ ]
+        listaDeTareasCompletas = [ ]
+    }
+}
+
+function guardarEnLS() {
+    localStorage.setItem("listaDeTareas", JSON.stringify(listaDeTareas))
+    localStorage.setItem("listaDeTareasCompletas", JSON.stringify(listaDeTareasCompletas))
+}
+check()
+function recargaDOM(){
+    listaDeTareas.forEach((tarea) => {
+        const item = document.createElement("li");
+        item.innerHTML =
+            tarea.actividad +
+            " (Dificultad : " +
+            tarea.dificultad +
+            ") " +
+            "<input class='form-check-input' type='checkbox' name='asunto'></input>";
+        let checkbox = item.querySelector("input[type='checkbox']");
+        checkbox.addEventListener("click", function () {
+            if (checkbox.checked) {
+                item.classList.add("completado");
+                deleteBtn.style.display = "none"
+                tareasCompletas.append(item)
+    
+                // Mover la tarea de listaDeTareas a listaDeTareasCompletas
+                const tareaCompletada = listaDeTareas.splice(listaDeTareas.indexOf(tarea), 1)[0];
+                listaDeTareasCompletas.push(tareaCompletada);
+                guardarEnLS()
+                console.log(listaDeTareasCompletas)
+            } else {
+                item.classList.remove("completado");
+                deleteBtn.style.display = "inline"
+                listaTareas.append(item)
+                // Mover la tarea de listaDeTareasCompletas a listaDeTareas
+                const tareaIncompleta = listaDeTareasCompletas.splice(listaDeTareasCompletas.indexOf(tarea), 1)[0];
+                listaDeTareas.push(tareaIncompleta);
+                guardarEnLS()
+                console.log(listaDeTareas)
+            }
+            onOffDeleteBtn();
+        });
+        const deleteBtn = document.createElement("i");
+        deleteBtn.className = "fa-solid fa-delete-left fa-2xl";
+        deleteBtn.addEventListener("click", (e) => {
+            const item = e.target.parentElement;
+            item.remove(item);
+            listaDeTareas.splice(
+                listaDeTareas.findIndex((t) => t.actividad === tarea.actividad),
+                1
+            )[0];
+            onOffDeleteBtn();
+            guardarEnLS()
+        });
+        item.append(deleteBtn);
+        listaTareas.append(item);
+        onOffDeleteBtn();
+    });
+    
+
+    listaDeTareasCompletas.forEach((tarea) => {
+        const item = document.createElement("li");
+        item.classList.add("completado")
+        item.innerHTML =
+            tarea.actividad +
+            " (Dificultad : " +
+            tarea.dificultad +
+            ") " +
+            "<input class='form-check-input' type='checkbox' checked name='asunto'></input>";
+        let checkbox = item.querySelector("input[type='checkbox']");
+        checkbox.addEventListener("click", function () {
+            if (checkbox.checked) {
+                item.classList.add("completado");
+                deleteBtn.style.display = "none"
+                tareasCompletas.append(item)
+    
+                // Mover la tarea de listaDeTareas a listaDeTareasCompletas
+                const tareaCompletada = listaDeTareas.splice(listaDeTareas.indexOf(tarea), 1)[0];
+                listaDeTareasCompletas.push(tareaCompletada);
+                guardarEnLS()
+            } else {
+                item.classList.remove("completado");
+                deleteBtn.style.display = "inline"
+                listaTareas.append(item)
+                item.append(deleteBtn)
+                // Mover la tarea de listaDeTareasCompletas a listaDeTareas
+                const tareaIncompleta = listaDeTareasCompletas.splice(listaDeTareasCompletas.indexOf(tarea), 1)[0];
+                listaDeTareas.push(tareaIncompleta);
+                guardarEnLS()
+            }
+            onOffDeleteBtn();
+        });
+        const deleteBtn = document.createElement("i");
+        deleteBtn.className = "fa-solid fa-delete-left fa-2xl";
+        deleteBtn.addEventListener("click", (e) => {
+            const item = e.target.parentElement;
+            item.remove(item);
+            listaDeTareas.splice(
+                listaDeTareas.findIndex((t) => t.actividad === tarea.actividad),
+                1
+            )[0];
+            onOffDeleteBtn();
+            guardarEnLS()
+        });
+        ;
+        tareasCompletas.append(item);
+        onOffDeleteBtn();
+        guardarEnLS()
+    });
+} 
 
 
 function agregarTarea(e) {
@@ -12,13 +139,16 @@ function agregarTarea(e) {
     deleteBtn.className = "fa-solid fa-delete-left fa-2xl";
     deleteBtn.addEventListener("click", (e) => {
         const item = e.target.parentElement;
-        item.remove(item);
-        onOffDeleteBtn()
+        item.remove(item)
+        listaDeTareas.splice(listaDeTareas.findIndex(t => t.actividad === tarea.actividad), 1)[0];
+        guardarEnLS();
+        onOffDeleteBtn();
     });
     const tarea = {
         actividad: inputTarea.value,
         dificultad: selectorDificultad.value,
     }
+    listaDeTareas.push(tarea)
     let item = document.createElement("li")
     item.innerHTML = tarea.actividad + " (Dificultad : " + tarea.dificultad + ") " + "<input class='form-check-input' type='checkbox' name='asunto'></input>";
     let checkbox = item.querySelector("input[type='checkbox']");
@@ -27,37 +157,53 @@ function agregarTarea(e) {
             item.classList.add("completado");
             deleteBtn.style.display = "none"
             tareasCompletas.append(item)
+
+            // Mover la tarea de listaDeTareas a listaDeTareasCompletas
+            const tareaCompletada = listaDeTareas.splice(listaDeTareas.indexOf(tarea), 1)[0];
+            listaDeTareasCompletas.push(tareaCompletada);
+            guardarEnLS()
         } else {
             item.classList.remove("completado");
             deleteBtn.style.display = "inline"
             listaTareas.append(item)
+            // Mover la tarea de listaDeTareasCompletas a listaDeTareas
+            const tareaIncompleta = listaDeTareasCompletas.splice(listaDeTareasCompletas.indexOf(tarea), 1)[0];
+            listaDeTareas.push(tareaIncompleta);
+            guardarEnLS()
         }
         onOffDeleteBtn()
     })
     onOffDeleteBtn()
+
+
     item.append(deleteBtn)
     listaTareas.append(item)
     inputTarea.focus()
     btnAgregar.reset()
-    
+    guardarEnLS()
+    console.log(listaDeTareas)
+    console.log(listaDeTareasCompletas)
 }
 
-function deleteAll(){
+function deleteAll() {
     const items = document.querySelectorAll("li.completado");
     items.forEach((item) => {
         item.remove();
     });
-    onOffDeleteBtn()
+    listaDeTareasCompletas.forEach(() => {
+        listaDeTareasCompletas.length = 0;
+    });
+    guardarEnLS()
+    onOffDeleteBtn();
 }
 
-function onOffDeleteBtn(){
+function onOffDeleteBtn() {
     if (tareasCompletas.children.length > 0) {
         xBtn.style.display = "initial";
-    } else{
+    } else {
         xBtn.style.display = "none";
     }
 }
-
 
 
 btnAgregar.addEventListener("submit", agregarTarea)
