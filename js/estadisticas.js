ce67e70c2d930e17e89bdba2dfd65b5b180b44e4
Agustin Torres Valenzuela <1997.agustorres@gmail.com>
2023-05-24 01:38:34 -0300

    estadisticas

---
+++ js/estadisticas.js
@@ -0,0 +1,6 @@
+const estadisticasPokedex = document.querySelector(".estadisticas-pokedex");
+const contadorNormales = document.querySelector(".contador-normales");
+const contadorHonor = document.querySelector(".contador-honor");
+const contadorSuper = document.querySelector(".contador-super");
+const contadorUltra = document.querySelector(".contador-ultra");
+const contadorMaster = document.querySelector(".contador-master");
